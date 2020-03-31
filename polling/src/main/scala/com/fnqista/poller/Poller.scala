ec2503c24c4d3ae93f8fff5f68a44112c74316f7
fnqista <ailveen@yahoo.com>
2020-03-31 16:17:41 +0200

    File Split sample

--- polling/src/main/scala/com/fnqista/poller/Poller.scala
+++ polling/src/main/scala/com/fnqista/poller/Poller.scala
@@ -1,35 +1,58 @@
 package com.fnqista.poller
 
+import java.io.{ ByteArrayInputStream, File, FileInputStream, InputStream }
+
 import cats.effect.IO.Async
 import monix.eval.Task
 import monix.execution.CancelableFuture
 import monix.execution.Scheduler.Implicits.global
 import monix.reactive.observers.Subscriber
 import monix.reactive.observers.Subscriber.Sync
-import monix.reactive.{Consumer, Observable, OverflowStrategy}
+import monix.reactive.{ Consumer, Observable, OverflowStrategy }
 
 import concurrent.duration._
 import scala.concurrent.Await
-import scala.util.{Random, Try}
+import scala.util.{ Random, Try }
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 object Poller extends App {
-  def rand(low: Int, high: Int): Int = {
-    val res = Random.nextInt((high - low) + 1) + low
-    if (res == 1) throw new RuntimeException("failed")
-    res
+
+  def writeChunk(byteArray: Array[Byte], index: Long): Unit = {
+    val outFile = new File(f"/tmp/test-large-out/1Gfile.part.$index%03d")
+    val outStream: OutputStream = new FileOutputStream(outFile)
+    outStream.write(byteArray)
   }
 
-  private val future = Observable.eval(rand(1, 3))
-    .dump("out")
-    .delayExecution(2.seconds)
-    .restartUntil(_ == 3)
-    .onErrorRestart(3)
-    .firstL
+  val fileStream = new FileInputStream(new File("/tmp/test-large/1Gfile"))
+
+  val x = Observable
+    .fromInputStream(Task { fileStream }, chunkSize = 100 * 100 * 100 * 100)
+    .zipWithIndex
+    .mapParallelUnordered(12) {
+      case (byteArray, index) => Task { writeChunk(byteArray, index) }
+    }
+    .completedL
     .runToFuture
 
-  Await.result(future, 100.seconds)
+  Await.result(x, 23.hours)
 
-
+//  def rand(low: Int, high: Int): Int = {
+//    val res = Random.nextInt((high - low) + 1) + low
+//    if (res == 1) throw new RuntimeException("failed")
+//    res
+//  }
+//
+//  private val future = Observable
+//    .eval(rand(1, 3))
+//    .dump("out")
+//    .delayExecution(2.seconds)
+//    .restartUntil(_ == 3)
+//    .onErrorRestart(3)
+//    .firstL
+//    .runToFuture
+//
+//  Await.result(future, 100.seconds)
   //  Observable.create(OverflowStrategy.Unbounded) {
   //    sub => Task.deferFuture(sub.onNext(rand(1, 5)))
   //        .runToFuture(sub.scheduler)
@@ -45,8 +68,6 @@ object Poller extends App {
   //
   //  val res = task.consumeWith(Consumer.foreach[Int](i => Console println i))
   //  Await.result(res.runToFuture, 10.seconds)
-
-
   //  val task = Observable.fromIterable(1 to 3)
   //    .map(i => i + 2)
   //    .map(i => i * 3)
